3b202d558082cffc63615c1d227b1a58bf7a62e2
aaksant <aaksangit@gmail.com>
2024-01-30 21:43:44 +0700

    Add early implementation of check winner functionality

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
-// TODO: complete basic gameplay
+// TODO: 1 - DONE - Complete basic gameplay
+// TODO: 2 - DONE - Add check winner feature
 const init = (() => {
-  const board = ['', '', '', '', '', '', '', '', ''];
+  const filledBox = ['', '', '', '', '', '', '', '', ''];
   const boxNodes = document.querySelectorAll('.box');
-  let currentMark = 'X';
+  let currentMove = 'X';
+  let isWin = false;
 
   const fillBox = () => {
     boxNodes.forEach((box, i) => {
       box.addEventListener('click', () => {
-        if (board[i] === '') {
-          board[i] = currentMark;
-          box.textContent = currentMark;
-          currentMark = (currentMark === 'X') ? 'O' : 'X';
+        if (filledBox[i] === '') {
+          filledBox[i] = currentMove;
+          box.textContent = currentMove;
+          currentMove = (currentMove === 'X') ? 'O' : 'X';
+          checkWinner();
         }
       });
     });
+  };
+
+  const checkWinner = () => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    winConditions.forEach((condition) => {
+      if (
+        filledBox[condition[0]] === filledBox[condition[1]] &&
+        filledBox[condition[1]] === filledBox[condition[2]] &&
+        filledBox[condition[0]] !== ''
+      ) {
+        isWin = true;
+      }
+    });
+
+    return isWin;
   };
 
   return { fillBox };
